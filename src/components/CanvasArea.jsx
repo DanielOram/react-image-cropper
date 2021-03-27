89748a4bb9c5585831094bdecb65c5a3112bec3a
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 18:57:53 +0000

    Add event handler for changing cropper image

--- src/components/CanvasArea.jsx
+++ src/components/CanvasArea.jsx
@@ -11,9 +11,26 @@ const defaultSrc = "https://raw.githubusercontent.com/roadmanfong/react-cropper/
 export default function CanvasArea() {
     // CanvasArea is in charge of handling all data used by the imagecropper and the preview component
     const [image, setImage] = useState(defaultSrc);
+
+    const onChange = (event) => {
+        event.preventDefault();
+        let files;
+        if (event.dataTransfer) {
+            files = event.dataTransfer.files;
+        } else if (event.target) {
+            files = event.target.files;
+        }
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImage(reader.result);
+        };
+        reader.readAsDataURL(files[0]);
+    };
+    
     return (
         <div>
             <h2>Canvas Area</h2>
+            <input type="file" onChange={onChange} />
             <Cropper
                 src={image}
                 style={{ height: 400, width: "100%" }} />
