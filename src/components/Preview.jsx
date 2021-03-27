9322dcabad56fc08f4fde8dd61d1cee7363cecc3
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 22:10:06 +0000

    center preview inside containing element

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export default function Preview() {
+    const previewWidth = 200;
+    const previewHeight = 200;
+
     return (
-        <div className="crop-preview"
-        style={{ width: "300px", height: "300px", overflow: "hidden", maxHeight: 300, float: "right" }}>
+        <div style={{backgroundColor: "green", width: "100%", height: 200, float: "right", display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <div 
+                className="crop-preview"
+                style={{width: "100%", height: "inherit", overflow: "hidden", maxHeight: "100%" }}>
+            </div>
         </div>
         
     )
