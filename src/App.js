cf94890e0a5d7f90de9d5d0492dc35485011a506
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 18:13:24 +0000

    Create CanvasArea component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
+import CanvasArea from './components/CanvasArea';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <h1>React Image Cropper</h1>
+      <CanvasArea />
       <Footer />
     </div>
   );
---
+++ src/components/CanvasArea.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+export default function CanvasArea() {
+    return (
+        <h2>Canvas Area</h2>
+    )
+}
