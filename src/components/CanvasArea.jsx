06c36427190983148da931bcb4bcd7eb7b0cc345
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 18:19:03 +0000

    Create DownLoadModal component

--- src/components/CanvasArea.jsx
+++ src/components/CanvasArea.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Preview from './Preview';
+import DownLoadModal from './DownLoadModal';
 
 export default function CanvasArea() {
     return (
         <div>
             <h2>Canvas Area</h2>
             <Preview />
+            <DownLoadModal />
         </div>
     )
 }
---
+++ src/components/DownLoadModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DownLoadModal() {
+    return (
+        <div>
+            <h3>DownLoad Modal</h3>
+        </div>
+    )
+}
