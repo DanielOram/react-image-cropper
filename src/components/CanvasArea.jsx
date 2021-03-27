8cc4862556069a82a64cc127f5e4bb7bbcd711e6
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 18:41:35 +0000

    Add cropper to canvas area with default image

--- src/components/CanvasArea.jsx
+++ src/components/CanvasArea.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
 import Preview from './Preview';
 import DownLoadModal from './DownLoadModal';
 
+const defaultSrc = "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
+
 export default function CanvasArea() {
     // CanvasArea is in charge of handling all data used by the imagecropper and the preview component
-
+    const [image, setImage] = useState(defaultSrc);
     return (
         <div>
             <h2>Canvas Area</h2>
+            <Cropper
+                src={image}
+                style={{ height: 400, width: "100%" }} />
             <Preview />
             <DownLoadModal />
         </div>
