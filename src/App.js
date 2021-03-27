cecbc4c10eb70b817fe734fe629c131c72e8a2c2
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 17:54:29 +0000

    Create NavBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>React Image Cropper</h1>
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo center">Logo</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
