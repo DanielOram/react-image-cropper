e95180015df5e55362cd6e71626a4f2c3613d9c7
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 18:31:21 +0000

    Update CanvasArea.jsx

--- src/components/CanvasArea.jsx
+++ src/components/CanvasArea.jsx
@@ -3,6 +3,8 @@ import Preview from './Preview';
 import DownLoadModal from './DownLoadModal';
 
 export default function CanvasArea() {
+    // CanvasArea is in charge of handling all data used by the imagecropper and the preview component
+
     return (
         <div>
             <h2>Canvas Area</h2>
