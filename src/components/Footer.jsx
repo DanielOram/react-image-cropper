18bf6385d6d7a7d998654b185710b24c87fed7dd
Daniel Oram <danieloramnz@gmail.com>
2021-03-27 18:57:38 +0000

    change jsx class attributes to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer class="page-footer">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Created by Daniel Oram.</h5>
-                <p class="grey-text text-lighten-4">A webapp for cropping and downloading images using React.</p>
+        <footer className="page-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col l6 s12">
+                <h5 className="white-text">Created by Daniel Oram.</h5>
+                <p className="grey-text text-lighten-4">A webapp for cropping and downloading images using React.</p>
               </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
+              <div className="col l4 offset-l2 s12">
+                <h5 className="white-text">Links</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
                 </ul>
               </div>
             </div>
           </div>
-          <div class="footer-copyright">
-            <div class="container">
+          <div className="footer-copyright">
+            <div className="container">
             © 2021 Daniel Oram
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
             </div>
           </div>
         </footer>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function NavBar() {
     return (
         <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo center">React Image Cropper</a>
-                <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo center">React Image Cropper</a>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
                     <li><a href="sass.html">Sass</a></li>
                     <li><a href="badges.html">Components</a></li>
                     <li><a href="collapsible.html">JavaScript</a></li>
