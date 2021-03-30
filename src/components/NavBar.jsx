83223070599c3b684c9c028cd8bd7ac4fa52c5fe
Daniel Oram <danieloramnz@gmail.com>
2021-03-30 18:43:18 +0100

    add href links to github repo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ export default function NavBar() {
                     <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="#">About Me</a></li>
-                        <li><a href="#">Github</a></li>
+                        <li><a href="https://github.com/DanielOram/react-image-cropper">Github</a></li>
                         <li><a href="https://stackoverflow.com/users/4486146/danoram">StackOverflow</a></li>
                     </ul>
                 </div>
@@ -20,7 +20,7 @@ export default function NavBar() {
             <ul class="sidenav" id="mobile-demo">
                 <li><a data-target="modal1" class="waves-effect waves-light btn btn-large modal-trigger"><span>How to Use<i class="material-icons right">help_outline</i></span></a></li>
                 <li><a href="#">About Me<i class="material-icons right">info_outline</i></a></li>
-                <li><a href="#">Github<i class="material-icons right">code</i></a></li>
+                <li><a href="https://github.com/DanielOram/react-image-cropper">Github<i class="material-icons right">code</i></a></li>
                 <li><a href="https://stackoverflow.com/users/4486146/danoram">StackOverflow<i class="material-icons right">code</i></a></li>
             </ul>
         </>
