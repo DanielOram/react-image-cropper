5c18be48984f43872ee0b8730ba12d4a29361c33
Daniel Oram <danieloramnz@gmail.com>
2021-03-31 11:13:52 +0100

    Update Preview.jsx

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export default function Preview() {
-    const previewWidth = 200;
-    const previewHeight = 200;
 
     return (
-        <div className="crop-preview-container" style={{ width: "100%", height: 200, float: "right", display: "flex", alignItems: "center", justifyContent: "center"}}>
-            <div 
-                className="crop-preview"
-                style={{width: "100%", height: "inherit", overflow: "hidden", maxHeight: "100%" }}>
+        <>
+            <div className="crop-preview-container" style={{ width: "100%", height: 200, float: "right", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <div 
+                    className="crop-preview"
+                    style={{width: "100%", height: "inherit", overflow: "hidden", maxHeight: "100%" }}>
+                </div>
             </div>
-        </div>
+        </>
         
     )
 }
